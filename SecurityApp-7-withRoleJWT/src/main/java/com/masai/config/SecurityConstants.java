3c2e7bda574023952dc0073418e007cb6ec438b7
MrVivek30 <vivekchaudhary538@gmail.com>
2023-07-10 03:28:10 +0530

    Role_JWT

---
+++ SecurityApp-7-withRoleJWT/src/main/java/com/masai/config/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.masai.config;
+
+public interface SecurityConstants {
+//by default public static final
+	
+	public static final String JWT_KEY="vgswdwghkdgefgghdgdhcvhbcdjwhidjwdwdwddvfwdv";
+	public static final String JWT_HEADER="Authorization";
+}
